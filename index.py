4d88b7f82760a25842249721ed3a038db3d6ca3a
FerreWagner <18408229270@163.com>
2019-05-09 23:50:33 +0800

    webpy accept form params

--- index.py
+++ index.py
@@ -2,6 +2,8 @@ import web
         
 urls = (
 	'/blog/\d+', 'blog',
+	'/rape/(.*)', 'rape',
+	'/paramss', 'accepts',
 	'/(.*)', 'hello',
 )
 app = web.application(urls, globals())
@@ -15,6 +17,17 @@ class hello:
 class blog:
 	def GET(self):
 		return 'number:'
+
+class rape:
+	def GET(self, query):
+		if not query:
+			query = 'no'
+		return 'Freeze' + query
+		
+class accepts:
+	def GET(self):
+		query = web.input()
+		return query
 
 if __name__ == "__main__":
     app.run()
